96e47a834e3063dd91c9e8b71845d13e02fe4a75
Andrey Zotov <aszotov@avito.ru>
2023-03-19 04:05:44 +0300

    small cli app with tests

---
+++ data_structures/trees_and_graphs/graphs/dfs/number_of_provinces/number_of_provinces.go
@@ -0,0 +1,16 @@
+package main
+
+//import (
+//	"fmt"
+//	"trees_and_graphs/graphs"
+//)
+//
+//func numberOfProvinces(cities *graphs.Graph) int {
+//
+//}
+//
+//func main() {
+//	cities := [][]int{{1,7}, {1,3}, {4,2}, {5,6}, {0}}
+//	g := graphs.NewUnDirectlyGraph(cities)
+//	fmt.Println(numberOfProvinces(g))
+//}
